935d0120198dd3f4edd7f039edc15b4d9d76ec25
Violetta Akura <balakina.vi@ya.ru>
2026-09-19 12:52:47 +0900

    форма работает и не работает кнопка сбросить

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,31 +1,119 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import {
+	ArticleStateType,
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from 'src/constants/articleProps';
+import { Text } from 'src/ui/text';
 import { useState } from 'react';
 import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articleState: ArticleStateType;
+	setArticleState: (state: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	articleState,
+	setArticleState,
+}: ArticleParamsFormProps) => {
 	const [isOpen, setIsOpen] = useState(false);
+
+	const [formState, setFormState] = useState<ArticleStateType>(articleState);
+
+	const updatedField = <K extends keyof ArticleStateType>(
+		field: K,
+		value: ArticleStateType[K]
+	) => {
+		setFormState((prev) => ({ ...prev, [field]: value }));
+	};
+
+	const handleApply = () => {
+		setArticleState(formState);
+		setIsOpen(false);
+	};
+
+	const handleReset = () => {
+		setFormState(articleState);
+	};
 
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen((prev) => !prev)} />
+
 			<aside
-				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
-				<form className={styles.form}>
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={(e) => {
+						e.preventDefault();
+						handleApply();
+					}}>
+					<Text as={'h2'} size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) => updatedField('fontFamilyOption', option)}
+					/>
+
+					<RadioGroup
+						name='fontSize'
+						title='Размер шрифта'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(option) => updatedField('fontSizeOption', option)}
+					/>
+
+					<Select
+						title='Цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(option) => updatedField('fontColor', option)}
+					/>
+
+					<Separator />
+
+					<Select
+						title='Цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => updatedField('backgroundColor', option)}
+					/>
+
+					<Select
+						title='Ширина контента'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => updatedField('contentWidth', option)}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button
 							title='Сбросить'
 							htmlType='reset'
 							type='clear'
-							onClick={() => setIsOpen(false)}
+							onClick={handleReset}
 						/>
 						<Button
 							title='Применить'
 							htmlType='submit'
 							type='apply'
-							onClick={() => setIsOpen(false)}
+							onClick={handleApply}
 						/>
 					</div>
 				</form>
